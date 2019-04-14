92d8c21cbb8a725d1d22cff8ffd66c172c5ffbda
Eugeny Popov <>
2019-04-14 14:52:35 -0400

    Add framework

--- IronSourceCarthage/IronSourceCarthage/IronSourceCarthage.h
+++ IronSourceCarthage/IronSourceCarthage/IronSourceCarthage.h
@@ -17,3 +17,21 @@ FOUNDATION_EXPORT const unsigned char IronSourceCarthageVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <IronSourceCarthage/PublicHeader.h>
 
 
+#import "IronSource.h"
+//#import "ISGender.h"
+//#import "ISBannerDelegate.h"
+//#import "ISRewardedVideoDelegate.h"
+//#import "ISOfferwallDelegate.h"
+//#import "ISInterstitialDelegate.h"
+//#import "ISRewardedInterstitialDelegate.h"
+//#import "ISLogDelegate.h"
+//#import "ISConfigurations.h"
+//#import "ISPlacementInfo.h"
+//#import "ISIntegrationHelper.h"
+//#import "ISEventsReporting.h"
+//#import "ISSupersonicAdsConfiguration.h"
+//#import "ISSegment.h"
+//#import "ISSegmentDelegate.h"
+//#import "ISDemandOnlyRewardedVideoDelegate.h"
+//#import "ISDemandOnlyInterstitialDelegate.h"
+//#import "ISBannerSize.h"
---
+++ IronSourceCarthage/IronSourceCarthage/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  IronSourceCarthage
+//
+//  Created by Eugeny Popov on 4/14/19.
+//  Copyright © 2019 Eugeny Popov. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    func test() {
+        // Test line
+        IronSource.setAdaptersDebug(true)
+    }
+}
