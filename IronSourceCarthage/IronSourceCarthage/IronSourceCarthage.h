a919f39edd025832cf3b87fe29eca550bf771e5f
Eugeny Popov <>
2019-04-14 13:01:32 -0400

    Clean

---
+++ IronSourceCarthage/IronSourceCarthage/IronSourceCarthage.h
@@ -0,0 +1,19 @@
+//
+//  IronSourceCarthage.h
+//  IronSourceCarthage
+//
+//  Created by Eugeny Popov on 4/14/19.
+//  Copyright © 2019 Eugeny Popov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IronSourceCarthage.
+FOUNDATION_EXPORT double IronSourceCarthageVersionNumber;
+
+//! Project version string for IronSourceCarthage.
+FOUNDATION_EXPORT const unsigned char IronSourceCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IronSourceCarthage/PublicHeader.h>
+
+
